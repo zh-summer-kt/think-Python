4a48d0b0443803d2240e4c12681bf35b3ca1d4d0
KTZH <zh_summer@hotmail.com>
2017-04-06 10:29:19 +0800

    start learning think-python Apr 6

---
+++ exercise-print_grid.py
@@ -0,0 +1,20 @@
+def print_grid():
+	print_outter()
+	print_inner()
+	
+def print_outter():
+	print ("+"+"-"*4)*4+"+"
+	
+def print_inner():
+	print ("|"+" "*4)*4+"|"
+	
+def one_row():
+	print_outter()
+	print_inner()
+
+one_row()
+one_row()
+one_row()
+one_row()
+print_outter()
+
